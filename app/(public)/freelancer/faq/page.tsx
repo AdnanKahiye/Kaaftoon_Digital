4da073d512f92dcc5e4961395e045b7bed3d24ff
Eng Kahiye <kahiye@gmail.com>
2026-02-08 05:49:15 -0800

    updated

--- app/(public)/freelancer/faq/page.tsx
+++
@@ -1,8 +0,0 @@
-import FreelancerPage from "@/components/freelancer/FreelancerPage";
-import { freelancerPages } from "@/lib/freelancer";
-
-export default function FreelancerFaqPage() {
-  return (
-    <FreelancerPage {...freelancerPages.faq} />
-  );
-}
--- app/(public)/freelancer/join/page.tsx
+++
@@ -1,8 +0,0 @@
-import FreelancerPage from "@/components/freelancer/FreelancerPage";
-import { freelancerPages } from "@/lib/freelancer"
-
-export default function JoinFreelancerPage() {
-  return (
-    <FreelancerPage {...freelancerPages.join} />
-  );
-}
