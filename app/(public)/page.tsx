29ee1d33ccd381fc2a7c35961fea93027f1a0ff0
Adnan Kahiye <cadnansaid470@gmail.com>
2026-03-07 01:07:51 -0800

    add  deployment

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -9,7 +9,7 @@ import ContactSection from "@/components/sections/ContactSection";
 
 export default function HomePage() {
   return (
-    <main>
+    <main className="overflow-x-hidden">
       {/* HERO */}
       <HeroSection />
 
